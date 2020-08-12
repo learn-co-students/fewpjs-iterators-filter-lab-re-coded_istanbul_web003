745c494f987f7c156a8f74a60e5b7a9973fa0a75
orcunsagirsoy <orcunsagirsoy@gmail.com>
2020-08-12 21:09:40 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+function findMatching(drivers, string){
+
+    const result = drivers.filter( driver => driver.toLowerCase() === string.toLowerCase());
+
+    return result;
+
+}
+
+
+function fuzzyMatch(drivers, string){
+
+    const result = drivers.filter(driver => driver.substring(0,2) === string);
+
+    return result;
+
+
+}
+
+
+function matchName(drivers, string){
+
+    const result = drivers.filter(driver => driver.name === string);
+
+    return result;
+
+
+}
