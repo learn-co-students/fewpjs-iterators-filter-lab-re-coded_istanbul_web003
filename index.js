07c0cbb161f7561996f1ec16b59701babec1aaba
mohammad-jundi <mohammad.jundi1990@gmail.com>
2020-08-23 18:14:23 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 // Code your solution here
-function findMatching(drivers, "str"){
-    
+function findMatching(drivers, str){
+    let results = drivers.filter(driver=>driver.toUpperCase()===str.toUpperCase());
+    if(results===[]){
+        return results=[]
+    }
+    return results
 }
-function fuzzyMatch(){
-
+function fuzzyMatch(drivers, driverName){
+    let results = drivers.filter(driver=>driver.charAt(0)===driverName.charAt(0));
+    return results; 
 }
 
-function matchName(){
-
+function matchName(drivers, name){
+    return drivers.filter(driver=>driver.name.toUpperCase()===name.toUpperCase())
 }
